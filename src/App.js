23dd10460d54df5fa237b6b68a0ae77b062ba2a0
ShaylaWhite <shayladwhite1@gmail.com>
2021-01-09 18:53:16 -0800

    index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+
 class App extends React.Component {
-  render() {
-    return {
-      <div className="App">
-      App
-      </div>
-    }
-  }
+
+render() {
+  return (
+    <div className="App">
+    App
+    </div>
+  );
 }
 
+}
 
 export default App;
