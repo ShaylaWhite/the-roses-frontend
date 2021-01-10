e120bb52151d9426ede3fb6ca7d273e3d5cb228a
ShaylaWhite <shayladwhite1@gmail.com>
2021-01-09 20:09:09 -0800

    gardens fetch testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React from 'react';
 
 class App extends React.Component {
 
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/gardens', {
+       method: 'GET'
+  })  
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
 render() {
   return (
     <div className="App">
